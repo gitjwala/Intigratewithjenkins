4ca6f129770aac4f99a997e87578f3b13e2b2914
gitjwala <151565079+gitjwala@users.noreply.github.com>
2023-12-05 17:37:47 -0600

    Update ec2.tf

--- modules/ec2.tf
+++ modules/ec2.tf
@@ -1,3 +1,7 @@
 resource "aws_instance" "my_instance"{
-    ami = "ami-089c26792dcb1fbd4"    
+    ami = "ami-089c26792dcb1fbd4"  
+    instance_type = var.instance_type
+tags = {
+        Name = "my-first-ec2"
+    }
 }
