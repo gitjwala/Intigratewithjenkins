fccee512a879bbe04704206211833326733b040c
gitjwala <151565079+gitjwala@users.noreply.github.com>
2023-12-06 17:26:04 -0600

    Update variable.tf

--- modules/variable.tf
+++ modules/variable.tf
@@ -1,15 +1,4 @@
-variable "instance_type" {
-  default = "t2.nano"   
-}
-
-variable "iam" {
-  default = "ami-089c26792dcb1fbd4"   
-}
-
-variable "sg_in_port" {
-  default = "80"
-}
-
-variable "sg_eg_port" {
-  default = "443"
-}
+variable "instance_type" {}
+variable "iam" {}
+variable "sg_in_port" {}
+variable "sg_eg_port" {}
