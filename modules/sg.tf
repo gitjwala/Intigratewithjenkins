78013393a39852984838193be069fb539d0ae26f
gitjwala <151565079+gitjwala@users.noreply.github.com>
2023-12-05 21:14:04 -0600

    Update sg.tf

--- modules/sg.tf
+++ modules/sg.tf
@@ -1,25 +1,20 @@
-resource "aws_security_group" "demo_sg" {
-  name        = "sample_sg"
+resource "aws_security_group" "mysg" {
+  name        = "mysg"
   description = "Allow TLS inbound traffic"
-  dynamic "ingress" {
-    for_each = var.sg_in_port
-    content {
-      description = "TLS from VPC"
-      from_port   = ingress.value
-      to_port     = ingress.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
+
+  ingress {
+    description      = "HTTPS inbount popt"
+    from_port        = var.sg_in_port
+    to_port          = var.sg_in_port
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
- dynamic "egress" {
-   for_each = var.sg_eg_port
-   content {
-    from_port        = egress.value
-    to_port          = egress.value
-    protocol         = "-1"
+  egress {
+    description      = "HTTP outbound Port"
+    from_port        = var.sg_eg_port
+    to_port          = var.sg_eg_port
+    protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
- }
 }
