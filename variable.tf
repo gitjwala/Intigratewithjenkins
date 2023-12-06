faffd47a7fe1539557b901b57805faedf7adc673
gitjwala <151565079+gitjwala@users.noreply.github.com>
2023-12-06 17:37:02 -0600

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,4 +1,12 @@
-variable "instance_type" {}
-variable "ami" {}
-variable "sg_in_port" {}
-variable "sg_eg_port" {}
+variable "instance_type" {
+type = "string"
+}
+variable "iam" {
+type = "string"
+}
+variable "sg_in_port" {
+type = "string"
+}
+variable "sg_eg_port" {
+type = "string"
+}
