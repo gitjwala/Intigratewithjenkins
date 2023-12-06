1c21b6c5e1aebfba4362ad5c9e376d0b5cab637e
gitjwala <151565079+gitjwala@users.noreply.github.com>
2023-12-05 19:40:00 -0600

    Update variable.tf

--- modules/variable.tf
+++ modules/variable.tf
@@ -1,3 +1,13 @@
 variable "instance_type" {
   default = "t2.nano"   
 }
+
+variable "sg_in_port" {
+  type    = list(number)
+  default = [8080, 8081, 8082, 8083]
+}
+
+variable "sg_eg_port" {
+  type    = list(number)
+  default = [80, 443, 8443]
+}
