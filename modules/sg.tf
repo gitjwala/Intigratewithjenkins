93521d4813e6ca312aff4c770b4135cc9f0e88ae
gitjwala <151565079+gitjwala@users.noreply.github.com>
2023-12-05 19:41:14 -0600

    Update sg.tf

--- modules/sg.tf
+++ modules/sg.tf
@@ -4,16 +4,16 @@ resource "aws_security_group" "ec2_sg" {
 
   ingress {
     description      = "HTTPS inbount popt"
-    from_port        = 80
-    to_port          = 80
+    from_port        = var.sg_in_port
+    to_port          = var.sg_in_port
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
     description      = "HTTP outbound Port"
-    from_port        = 443
-    to_port          = 443
+    from_port        = var.sg_eg_port
+    to_port          = var.sg_eg_port
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
