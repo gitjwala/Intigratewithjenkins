c5ab347d1b35eaa1791f574f73a8ce50d81d7389
gitjwala <151565079+gitjwala@users.noreply.github.com>
2023-12-06 19:34:11 -0600

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,12 +1,16 @@
 variable "instance_type" {
+description = "instance type"
 type = string
 }
 variable "ami" {
+description = "ID of EC2 instance"
 type = string
 }
 variable "sg_in_port" {
+description = "inbound value"
 default = "80"
 }
 variable "sg_eg_port" {
+description = "outbound value"
 default = "443"
 }
